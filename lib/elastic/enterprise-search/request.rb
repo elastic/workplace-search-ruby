96c7bec2caa851d82ac878ef3bdd02cdf2309a41
brianvans <bvans@swiftype.com>
2019-09-16 09:39:20 -0700

    Update the path to the ca bundle (#14)
    In 5251490d, some files got moved around and this relative path didn't
    get updated to reflect that, causing a regression with ssl-enabled
    endpoints

--- lib/elastic/enterprise-search/request.rb
+++ lib/elastic/enterprise-search/request.rb
@@ -50,7 +50,7 @@ module Elastic
             # is Charles, which uses a self-signed certificate in order to inspect https traffic. This will
             # not be part of this client's public API, this is more of a development enablement option
             http.verify_mode = ENV['st_ssl_verify_none'] == 'true' ? OpenSSL::SSL::VERIFY_NONE : OpenSSL::SSL::VERIFY_PEER
-            http.ca_file = File.join(File.dirname(__FILE__), '..', 'data', 'ca-bundle.crt')
+            http.ca_file = File.realpath(File.join(File.dirname(__FILE__), '..', '..', 'data', 'ca-bundle.crt'))
             http.ssl_timeout = open_timeout
           end
 
